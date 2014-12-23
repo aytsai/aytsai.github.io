8f6a406cd94d863ccbb6fe01231599a4b76b406f
Ariana Tsai <ariana.tsai@gmail.com>
2014-12-23 00:23:01 -0800

    updated some stuff

--- txtadvent.js
+++ txtadvent.js
@@ -1,6 +1,7 @@
 // to do:
 // link n <-> north, e <-> east, w <-> west, s <-> south
 // make use of typed.js and create occasional typos
+// check if an action contains string in dictionary
 
 /* Map
       ___ 
@@ -45,113 +46,127 @@ convo10 = "The ghost\'s eyes gloss over, but its hands continue to tippity tap a
 convo11 = "There seems to be a forcefield around the engineer that prevents you from giving the beard back.";
 
 act0 = "As you approach, it stares at you with blood-shot eyes... err, ghostly blood-shot eyes. Then it finally squeaks out, \"It's dangerous to go alone! Take this.\"";
-act1 = "You are suddenly inspired and stick the beard on the colon. It looks somewhat like a semicolon now. It seems absolutely delighted. You decide to take it along for a ride. Maybe it\'ll come in handy.";
+act1 = "A stroke of inspiration! You stick the beard on the colon. It looks somewhat like a semicolon now. The subject seems absolutely delighted. You decide to take it along for a ride. Maybe it\'ll come in handy.";
 
 map[0] = {
   visits: 0,
-	// if semicolon in inventory, display b2, else b0
-	b0: "You trudge westward. Illuminated by the faint glow of fireflies, a colon stares at you solemnly, stroking an imaginary beard.",
-	b1: "A vast emptiness stretche before you. A colon wearing a fake beard beams at you from your hand.",
-	conversation: [{"talk", convo6},
-								 {"north", convo4},
-								 {"west", convo4},
-								 {"south", convo4},
-								 {"firefly", convo7},
-								 {"fireflies", convo7}
+	b: function () {
+		   if (inventory.indexOf("semicolon") == -1)
+				 return "You trudge westward. Illuminated by the faint glow of fireflies, a colon stares at you solemnly, stroking an imaginary beard.";
+			 else
+				 return "A vast emptiness stretche before you. A colon wearing a fake beard beams at you from your hand."
+		 },
+	conversation: [{"talk": convo6},
+								 {"north": convo4},
+								 {"west": convo4},
+								 {"south": convo4},
+								 {"firefly": convo7},
+								 {"fireflies": convo7}
 								],
-	actions: [{"beard", null}
-					 ], // function to push colon into inventory
-	exits: [{"east", "1"}
+	actions: [{"beard": act0}
+					 ],
+	exits: [{"east": "1"}
 				 ]
 };
 
 map[1] = {
   visits: 0,
 	// if visits = 0, display b0, else if visits = 1, display b1, else display b2
-	b0: "It is very dark. If you continue, you are likely to be eaten by a bug." +
-			"<br/><br/>Suddenly, a voice booms out from above. \"<b><i>H-E-L-L-O</i></b>!! It's been a while since someone visited. I'm the ruler of this domain, and rules dictate that I must send unsuspecting travelers on a quest to save my kingdom. Here's your mission of the day - defeat the malevolent wandering bug in the dark! Aaany questions?\"",
-	b1: "You're about to say something when the voice butts in. \"Right, I forgot to mention... since 		there's no light in here, I set down some cookies so that you can find your way arou— why cookies?! Uh, it's definitely not because I accidentally spilled them when I turned the lights off... yeah, no way...\"" +
-			"<br/><br/>True to the voice's word, you sniff out three trails of cookies leading north, west, and east. <b>If you'd rather not be here, however, you can probably intimidate the voice into turning on the lights immediately to navigate the site.</b>",
-	b2: "You\'re in the room where you started. The three trails of cookies leading north, west, and east are still there, getting staler by the second. Somewhere above you, the voice is happily humming. You could intimidate it into letting you navigate the site immediately.",
-	conversation: [{"cookies", convo0}, // if input string contains the string, then accept
-								 {"cookie", convo0},
-								 {"eat", convo0},
-								 {"talk", convo0},
-								 {"south", convo4},
-								 {"semicolon", convo8},
-								 {"colon", convo8},
-								 {"beard", convo9}
+	b: function () {
+		   if (this.visits == 0)
+				 return "It is very dark. If you continue, you are likely to be eaten by a bug." +
+								"<br/><br/>Suddenly, a voice booms out from above. \"<b><i>H-E-L-L-O</i></b>!! It's been a while since someone visited. I'm the ruler of this domain, and rules dictate that I must send unsuspecting travelers on a quest to save my kingdom. Here's your mission of the day - defeat the malevolent wandering bug in the dark! Aaany questions?\"";
+			 else if (this.visits	== 1)			
+				 return "You're about to say something when the voice butts in. \"Right, I forgot to mention... since there's no light in here, I set down some cookies so that you can find your way arou— why cookies?! Uh, it's definitely not because I accidentally spilled them when I turned the lights off... yeah, no way...\"" +
+				 "<br/><br/>True to the voice's word, you sniff out three trails of cookies leading north, west, and east. <b>If you'd rather not be here, however, you can probably intimidate the voice into turning on the lights immediately to navigate the site.</b>";
+			 else
+				 return "You\'re in the room where you started. The three trails of cookies leading north, west, and east are still there, getting staler by the second. Somewhere above you, the voice is happily humming. You could intimidate it into letting you navigate the site immediately.";
+		 },
+	conversation: [{"cookies": convo0},
+								 {"cookie": convo0},
+								 {"eat": convo0},
+								 {"talk": convo0},
+								 {"south": convo4},
+								 {"semicolon": convo8},
+								 {"colon": convo8},
+								 {"beard": convo9}
 								],
 	actions: [],
-	exits: [{"west", "0"},
-					{"north", "4"},
-			   	{"east", "2"},
-					{"intimidate", "E"}
+	exits: [{"west": "0"},
+					{"north": "4"},
+			   	{"east": "2"},
+					{"intimidate": "E"}
 				 ]
 };
 
 map[2] = {
   visits: 0,
-	b0: "To the east, you find the ghost of an engineer feverishly working on its laptop.",
-	conversation: [{"talk", convo10},
-								 {"north", convo4},
-								 {"east", convo4},
-								 {"beard",},
-								 {"semicolon", convo10},
-								 {"colon", convo10}
+	b: "To the east, you find the ghost of an engineer feverishly working on its laptop.",
+	conversation: [{"talk": convo10},
+								 {"north": convo4},
+								 {"east": convo4},
+								 {"beard": convo10},
+								 {"semicolon": convo10},
+								 {"colon": convo10}
 								],
-	actions: [{"talk", null}
-					 ], // check actions for matching word first, function to push beard into inventory
-	exits: [{"west", "1"},
-					{"south", "3"}
+	actions: [{"talk": act1}
+					 ],
+	exits: [{"west": "1"},
+					{"south": "3"}
 				 ]
 };
 
 map[3] = {
   visits: 0,
-	b0: "In your haste to follow the trail of delicious cookies, you stumble into a shivering bug. A Malevolent Wandering Bug has appeared! ...or not. Contrary to what you heard before, the bug really seems quite harmless. It's carrying a teddybear in one arm and a crudely painted sign in the other.",
-	conversation: [{"teddybear", convo1},
-								 {"bear", convo1},
-								 {"sign", convo2},
-								 {"talk", convo3},
-								 {"west", convo4},
-								 {"south", convo4},
-								 {"east", convo4},
-								 {"beard", convo5}
+	b: "In your haste to follow the trail of delicious cookies, you stumble into a shivering bug. A Malevolent Wandering Bug has appeared! ...or not. Contrary to what you heard before, the bug really seems quite harmless. It's carrying a teddybear in one arm and a crudely painted sign in the other.",
+	conversation: [{"teddybear": convo1},
+								 {"bear": convo1},
+								 {"sign": convo2},
+								 {"talk": convo3},
+								 {"west": convo4},
+								 {"south": convo4},
+								 {"east": convo4},
+								 {"beard": convo5}
 								],
 	actions: [],
-	exits: [{"north", "2"},
-					{"semicolon", "E"},
-					{"colon", "E"}
+	exits: [{"north": "2"},
+					{"semicolon": "E"},
+					{"colon": "E"}
 				 ]
 };
 
 map[4] = {
   visits: 0,
-	b0: "You saunter north to find a huge \"UNDER CONSTRUCTION\" sign blocking your way.",
-	conversation: [{"north", convo4},
-								 {"west", convo4},
-								 {"east", convo4}
+	b: "You saunter north to find a huge \"UNDER CONSTRUCTION\" sign blocking your way.",
+	conversation: [{"north": convo4},
+								 {"west": convo4},
+								 {"east": convo4}
 								],
 	actions: [],
-	exits: [{"south", "1"}
+	exits: [{"south": "1"}
 				 ]
 };
 
+var caption = map[1].b;
+
 function getItem() {
-	if (current = 2) inventory.append ("beard");
-	else if (current = 0) inventory.append ("semicolon");
+	// check to see if beard room, if no beard already, or if already used on colon
+	if (room = 2 && (inventory.indexOf("beard") == -1 || inventory.indexOf("semicolon") >= 0)) {
+		inventory.append ("beard");
+		return false;
+	}
+	else if (room = 0 &&  inventory.indexOf("semicolon") == -1) {
+		inventory.append ("semicolon");
+		return false;
+	}
+	else return true; // already had or can't get item!!
 }
 
-/* Every time the viewer enters a new action, this function is called. First, the current
-   blurb is found, then a new blurb is created. The caption is set to the current one and
-   appended to the created blurb.
-*/
+/* Advance the story. */
 function advance() {
 	createBlurb();
-	caption = map[0].b;
 	$(function(){
-		$("#blurb" + j).typed({
+		$("#blurb" + numBlurb).typed({
 			strings: [caption],
 			typeSpeed: 5,
 			showCursor: true
@@ -159,8 +174,7 @@ function advance() {
 	});
 }
 
-/* A blurb is created.
-*/
+/* A blurb is created. */
 function createBlurb () {
   var area = document.getElementById("stuff");
 	var para = document.createElement("p");
@@ -173,7 +187,39 @@ function createBlurb () {
 	if (numBlurb != 0) $( "#blurbc" + (numBlurb-1) + " > span[class='typed-cursor']" ).remove();
 }
 
+/* Process input from action line. Exits take priority over actions, which take priority over conversations. */
 function process() {
-  var action = "";
-  action = $("input#aLine").val();
+  var act = "";
+	var flag = false;
+  act = $("input#aLine").val();
+	
+	// if just initialized, DON'T DO ANYTHING
+	if (room == 1 && map[room].visits == 0) {
+		map[room].visits++;
+		caption = map[room].b;
+	}
+	else if (act in map[room].exits) {
+		room = parseInt(map[room].exits.act);
+		map[room].vists++;
+		caption = map[room].b;
+	}
+	// getItem() will prevent item from being grabbed more than once
+	else if (act in map[room].actions) {
+		flag = getItem();
+		caption = map[room].actions.act;
+	}
+	
+	if (act in map[room].conversations && !flag) {
+		// just display blurb accordingly
+		// todo: there's some kind of logic i'm overlooking
+	}
+	$('#aLine').val('');
 }
+
+$("#aLine").keypress(function(e) {
+  code = e.keyCode;
+  if (code == 13) { // enter key is 13; allows user to submit action using enter
+    process();
+    advance();
+  } // if
+});
